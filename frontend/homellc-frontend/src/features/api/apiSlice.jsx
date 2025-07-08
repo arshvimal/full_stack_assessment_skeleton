6d289712993dcb1f388c69b5aa63c10ffafbd64e
Arsh Vimal <arsh11vimal@gmail.com>
2025-07-08 13:39:12 +0530

    pg sql file and backend api env variable

--- frontend/homellc-frontend/src/features/api/apiSlice.jsx
+++ frontend/homellc-frontend/src/features/api/apiSlice.jsx
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const homellcApi = createApi({
   reducerPath: 'homellcApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://127.0.0.1:3000' }),
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BACKEND_HOST + ":3000" }),
   keepUnusedDataFor: 300,
   tagTypes: ['User', 'Home'],
   endpoints: (builder) => ({
